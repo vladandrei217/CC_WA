57b9e00173c2e8352e2e4859265d402d8bde2ec0
Vlad.Petcu <Vlad.Petcu@PS044>
2024-11-07 08:55:08 +0200

    Add project files.

---
+++ CC_WA/Program.cs
@@ -0,0 +1,14 @@
+using CC_WA;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddRadzenComponents();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
